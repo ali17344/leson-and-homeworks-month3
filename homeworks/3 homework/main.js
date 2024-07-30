b3a61e7aa9345a11a8d9f94daafbb4413773a016
ali17344 <elokctr96@gmail.com>
2024-07-30 22:09:45 +0600

    4 leson

---
+++ homeworks/3 homework/main.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById('btn')
+let incoret = document.getElementById('incoret')
+let password = document.getElementById('password')
+let form = document.getElementById('form') 
+let result = /^123456789$/
+let finishResult = password.value.match(result)
+btn.addEventListener('click', () => {
+    if(password.value.match(result)){
+    open(form.action) 
+    } else{
+        incoret.textContent = 'Incoret password:123456789'
+    }
+  
+})
---
+++ leson/4 leson/3 lesson/assets/js/main.js
@@ -0,0 +1,39 @@
+
+// const xhr = new XMLHttpRequest() 
+ 
+// xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', true); 
+ 
+// xhr.send() 
+ 
+// console.log(xhr.responseText); 
+// xhr.onload = function(res){ 
+//     console.log('Запрос отправлен'); 
+//     console.log(xhr.responseText); 
+// }
+// const xhr_get_free_games = new XMLHttpRequest()
+// xhr_get_free_games.open('GET','https://www.freetogame.com/api/games',true)//настраиваем запрос
+// xhr_get_free_games.send()//отравляем запрос
+// xhr_get_free_games.onload = () => {
+//     console.log('Запрос отправлен !');
+ 
+// }
+fetch('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+    return response.json()
+}).then((data) => {
+    console.log(data);
+    let game_list = document.getElementsByClassName('game_lists');
+    let post = document.createElement('div')
+    for(post = data.title; post < 5; post++){
+    }
+    // post.innerText = data[0].title
+    // let post2 = document.createElement('div')
+    // post2.innerText = data[1].title
+    // let post3 = document.createElement('div')
+    // post3.innerText = data[2].title
+    // let post4 = document.createElement('div')
+    // post4.innerText = data[3].title
+    document.body.append(post)
+    // document.body.append(post2)
+    // document.body.append(post3)
+    // document.body.append(post4)
+})
