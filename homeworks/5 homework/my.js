895383008846e04931d03e355c2d291894bddcfe
ali17344 <elokctr96@gmail.com>
2024-08-16 19:08:03 +0600

    5 homework

--- homeworks/5 homework/my.js
+++ homeworks/5 homework/my.js
@@ -1,5 +1,6 @@
 const weather_form = document.getElementById('weather_form')
 console.log(weather_form);
+let btn = document.getElementById('btn')
 weather_form.addEventListener('submit', (event) => {
     event.preventDefault()
     const searchValue = document.getElementById('search')
@@ -7,7 +8,6 @@ weather_form.addEventListener('submit', (event) => {
        return response.json()
     }).then((data) =>{
         console.log(data);
-        let btn = document.getElementById('btn')
         let info = document.createElement('h1')
         let temp = document.getElementById('temp')
         let name = document.getElementById('Name')
@@ -18,29 +18,16 @@ weather_form.addEventListener('submit', (event) => {
         name.innerText = data.location.name
         wind_kph.innerText = data.current.wind_kph
         text.innerText = data.current.condition.text
-<<<<<<< HEAD
         temp.innerText =  data.current.temp_c
-=======
         temp.innerText = data.current.temp_c
->>>>>>> 99a1a237734f68a5c002a5b8dabf805b859b4c82
         btn.addEventListener('click', (event) => {
             event.preventDefault()
             fetch('http://api.weatherapi.com/v1/current.json?key=7013c0154c9a44a892430916230407&q='+searchValue.value).then((response) => {
                return response.json()
             }).then((data) =>{
-<<<<<<< HEAD
              if(temp.innerText == data.current.temp_c){
-=======
-            if(temp.innerText == data.current.temp_c){
->>>>>>> 99a1a237734f68a5c002a5b8dabf805b859b4c82
             temp.innerText =  data.current.temp_f
            }else if(temp.innerText == data.current.temp_f){
             temp.innerText = data.current.temp_c
            }
-<<<<<<< HEAD
         })})})})
-=======
-        })})
-    })
-})
->>>>>>> 99a1a237734f68a5c002a5b8dabf805b859b4c82
